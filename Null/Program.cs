3f5ffb3fab43bc6a79831dfda3f3f9e5526a9ed3
hasher116 <andrei13081996@mail.ru>
2021-08-06 16:17:30 +0300

    Добавьте файлы проекта.

---
+++ Null/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Null
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "andrew";
+            string z = name ?? "vasya";
+            Console.WriteLine(z);
+
+            User user = new User();
+            string companyName = user?.Phone?.Company?.Name ?? "не установлено";
+            Console.WriteLine(companyName);
+        }
+    }
+}
---
+++ Null/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Null
+{
+    class User
+    {
+        public Phone Phone { get; set; }
+    }
+    class Phone
+    {
+        public Company Company { get; set; }
+    }
+    class Company
+    {
+        public string Name { get; set; }
+    }
+}
